695e8e91a6b227995f40b15ff98b6efb9c82664f
ocefpaf <ocefpaf@gmail.com>
2014-07-25 09:08:22 -0300

    Removed temporary comparison R code.

--- co2sys/alkalinity.py
+++ co2sys/alkalinity.py
@@ -30,72 +30,10 @@ def conts_G(vol, pot, t):
         in situ temperature [:math:`^\circ` C (ITS-90)]
     """
 
-def at(S=35, T=25, C=0.1, d=1, pHTris=NULL, ETris=NULL, weight, E, volume):
-    r"""
-    S salinity    (constant)
-    T temperature (degC)  (vector or constant)
-    C molarity of acid   (constant)
-    d density of acid    (constant)
-    pHTris, pH used for the calibration of the electrode  (constant)
-    ETris, voltage used for the calibration of the electrode (constant)
-    E  voltage recorded during the titration in milliVolt(vector)
-    volume : volume of acid (vector)
-    weight : mass of the sample  (constant)
-    """
-
-    # Constants.
-    R, F = -8.31447215, -96485.339924
-
-    Tk = T + 273.15
-
-    # Creation of a table p.
-    p = data.frame(E=E, volume=volume, Tk=Tk)
-    z = p
-
-    # Transform mV in pH (total scale).
-    if (!is.null(pHTris) & !is.null(ETris)){
-        pH <- pHTris + (ETris/1000-E/1000)/(R*Tk*log(10)/F)
-
-    # creation of a table p
-    p <- data.frame(p, pH=pH)
-    #to use only the value between pH 3.5 and 3 (according to Dickson, 2007):
-    iii <- which((3<= p$pH)&(p$pH<=3.5))
-    z<- p[iii,]
-    }
-
-    options(digits=9)
-    m <- z$volume*d # Mass of acid
-    m0 <- weight    # Mass of the sample
-
-    #linear estimation of the total alkalinity (gran function):
-    F1<-(m0+m)*exp((z$E/1000)/(R*(z$Tk)/F))
-    f<-lm(m~F1)
-    TA<-f$coefficients[1]*C/m0[1]
-
-    #non linear estimation:
-    E0 <- z$E/1000-(R*z$Tk/F)*log((-m0*TA+m*C)/(m0+m))
-    Hprime <- exp((z$E/1000-E0)/(R*z$Tk/F))
-
-    St <- (0.14/96.062)*(S/1.80655)
-    Ks <- Ks(S,T[1], 0)
-    Z <- 1+ St/Ks
-    Ft <- (0.000067/18.998)*(S/1.80655)
-    Kf <- exp(874/z$Tk-9.68+0.111*S**(1/2))
-    y <- (m/m0)
-    regr <- nls(y ~ ((At + (St/(1 + Ks*Z/(f*Hprime)))+(Ft/ (1+Kf/(f*Hprime)))+(f*Hprime/Z))/(C-f*Hprime/Z)),start=list(At=TA, f=1))
-
-    ALK <- summary(regr)$parameters[1]
-    attr(ALK,"unit") <- "mol/kg-soln"
-    attr(ALK,"name") <- "Total Alkalinity"
-
-    # At= Total Alkalinity (mol/kg)
-    ALK = (100 * vol) * (10 ** ((pot + 96.4853 * 10 ** 3) / (np.log(10) * 8.31451 * 1000 * (t + K)))) * 0.001
-    return ALK
-
 # Using UNESCO 1983 (EOS 1980) polynomial.
 def d_smow(t):
-    r"""
-    Denisty of Standard Mean Ocean Water (Pure Water) using EOS 1980.
+    """
+    Density of Standard Mean Ocean Water (Pure Water) using EOS 1980.
 
     Parameters
     ----------
@@ -151,7 +89,7 @@ def d_smow(t):
 
 
 def dens0(s, t):
-    r"""Density of Sea Water at atmospheric pressure.
+    """Density of Sea Water at atmospheric pressure.
 
     Parameters
     ----------
